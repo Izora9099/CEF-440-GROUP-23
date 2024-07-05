3eb2d23b520805dcfc7368a9a24c705ef69f0aeb
Njidda Salifu <njiddasalifu@Njiddas-MacBook-Pro.local>
2024-07-05 15:44:58 +0100

    some enhances

--- source-code/PrintPass/Screens/Admin/AdminDashboard.js
+++ source-code/PrintPass/Screens/Admin/AdminDashboard.js
@@ -5,7 +5,7 @@ import { FontAwesome } from '@expo/vector-icons'; // Import FontAwesome icons
 import images from '../../constants/images';
 
 const data = [
-  { id: '1', title: 'Register Student', screenName: 'StudentRgisterScreen', pic: require('../../assets/images/add_student.png') },
+  { id: '1', title: 'Register Student', screenName: 'StudentRegisterScreen', pic: require('../../assets/images/add_student.png') },
   { id: '2', title: 'Create Session', screenName: 'SessionScreen',  pic: require('../../assets/images/session.png') },
   { id: '3', title: 'Generate Reports', screenName: 'ReportGeneratorScreen',  pic: require('../../assets/images/reports.png')  },
 ];
